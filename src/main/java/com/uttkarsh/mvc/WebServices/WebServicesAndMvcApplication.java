689f2ace80fe3acd6cd5e7151f3c394e7876313d
uttkarsh08 <uttkarshchugh01@gmail.com>
2025-03-11 00:05:02 +0530

    Initial commit

---
+++ src/main/java/com/uttkarsh/mvc/WebServices/WebServicesAndMvcApplication.java
@@ -0,0 +1,13 @@
+package com.uttkarsh.mvc.WebServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesAndMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesAndMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uttkarsh/mvc/WebServices/WebServicesAndMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uttkarsh.mvc.WebServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesAndMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
